af6db721a5fc6612bf9cbb6e46df3bebb9bae4b8
rova30 <rovarobel30@gmail.com>
2024-06-11 14:48:05 +0300

    contenu
    body, all-product, product-item

--- magic-vente-stock/src/app/app.module.ts
+++ magic-vente-stock/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './home/header/header.component';
 import { HomeComponent } from './home/home.component';
 import {FooterComponent} from "./home/footer/footer.component";
+import { BodyComponent } from './home/body/body.component';
+import { AllProductComponent } from './product/all-product/all-product.component';
+import { ProductItemComponent } from './product/product-item/product-item.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,9 @@ import {FooterComponent} from "./home/footer/footer.component";
     HeaderComponent,
     HomeComponent,
     FooterComponent,
+    BodyComponent,
+    AllProductComponent,
+    ProductItemComponent,
   ],
   imports: [
     BrowserModule,
